c169f5e50f80de408a8351a6c68c1204c4691db6
Damien Voindrot <damien.voindrot@link-value.fr>
2021-03-02 15:46:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/css/index.css";
 import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-/* import error404 from "./pages/404"; */
+import error404 from "./pages/404";
 import PrivateRoute from "./services/routes/PrivatesRoutes";
 import PublicRoute from "./services/routes/PublicRoutes";
 import Editor from "./pages/Editor";
@@ -28,7 +28,7 @@ const rootComponent = (
         <PublicRoute restricted path="/" exact component={Auth} />
         <PrivateRoute path="/dashboard" exact component={Home} />
         <PrivateRoute path="/editor" exact component={Editor} />
-        {/* <Route path="/" component={error404} /> */}
+        <Route path="/" component={error404} />
       </Switch>
     </Router>
   </Provider>

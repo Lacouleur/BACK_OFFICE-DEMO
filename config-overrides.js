f5536bf4a0f478493cd444e501f0a033f4c5333d
Pierre Pène <pierre.pene@gmail.com>
2021-04-12 16:39:50 +0200

    [PHX-000] CI by environment

--- config-overrides.js
+++ config-overrides.js
@@ -5,26 +5,15 @@ module.exports = function override(config) {
   if (!config.plugins) {
     config.plugins = [];
   }
-  if (process.env.NODE_ENV === `production`) {
-    config.plugins.push(
-      new webpack.DefinePlugin({ HOST_URL: JSON.stringify(`https://`) })
-    );
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        BASE_URL: JSON.stringify(``),
-      })
-    );
-  } else {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        HOST_URL: JSON.stringify(`http://localhost:3001`),
-      })
-    );
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        BASE_URL: JSON.stringify(`http://localhost:3011/api`),
-      })
-    );
-  }
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      HOST_URL: JSON.stringify(process.env.HOST_URL||'http://localhost:3001'),
+    })
+  );
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      BASE_URL: JSON.stringify(process.env.BASE_URL||'http://localhost:3011/api'),
+    })
+  );
   return config;
 };

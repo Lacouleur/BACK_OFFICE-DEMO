8900cac235dee7647a4a60f9fa692ce88ad70e02
Damien Voindrot <damien.voindrot@link-value.fr>
2021-05-06 12:08:47 +0200

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/css/index.css";
 import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-import error404 from "./pages/404";
+import Error404 from "./pages/404";
 import PrivateRoute from "./services/routes/PrivatesRoutes";
-import PublicRoute from "./services/routes/PublicRoutes";
 import Editor from "./pages/Editor";
 import combineReducers from "./store/combineReducers";
 import logMiddleware from "./store/logMiddleware";
@@ -25,10 +24,14 @@ const rootComponent = (
   <Provider store={store}>
     <Router>
       <Switch>
-        <PublicRoute restricted path="/" exact component={Auth} />
+        <Route path="/" exact>
+          <Auth />
+        </Route>
         <PrivateRoute path="/dashboard" exact component={Home} />
         <PrivateRoute path="/editor" exact component={Editor} />
-        <Route path="/" component={error404} />
+        <Route path="*">
+          <Error404 />
+        </Route>
       </Switch>
     </Router>
   </Provider>
